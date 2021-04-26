657d2f46931677efa577d2d9058abeb4dd85a059
edewiyoon <vva.varvara@gmail.com>
2021-04-25 19:47:19 -0700

    Assignment 2 is done c:

---
+++ Varvara_Vologdina_Assignment_2/Assets/Assignment2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float Run = 0.5f; 
+    private float nextRun;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextRun)
+        {
+            nextRun = Time.time + Run;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
